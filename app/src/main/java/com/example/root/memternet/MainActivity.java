e895a236e7a78ac255220179fcbd77b4079959e3
Nikita <warfacenikita@yandex.ru>
2018-01-14 09:59:19 +0300

    Meme.java class modified: getMemes static method now loads images consistently

--- app/src/main/java/com/example/root/memternet/MainActivity.java
+++ app/src/main/java/com/example/root/memternet/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.root.memternet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Meme> memes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                memes = Meme.getMemes(5, 3);
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView imageView1 = findViewById(R.id.imageView1);
+                ImageView imageView2 = findViewById(R.id.imageView2);
+                ImageView imageView3 = findViewById(R.id.imageView3);
+                imageView1.setImageBitmap(memes.get(0).getImage());
+                imageView2.setImageBitmap(memes.get(1).getImage());
+                imageView3.setImageBitmap(memes.get(2).getImage());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/root/memternet/Meme.java
+++ app/src/main/java/com/example/root/memternet/Meme.java
@@ -13,49 +13,43 @@ import java.util.List;
 
 public final class Meme {
 
+    static private ArrayList<Meme> memes;
     public final String url;
     public final long id;
     private Bitmap img;
 
-    Meme(long id, String url) {
+    public Meme(long id, String url, Bitmap bitmap) {
         this.id = id;
         this.url = url;
-        (new MemeDownloader(new MemeDownloader.OnImageComplete() {
-            @Override
-            public void onComplete(Bitmap image) {
-                img = image;
-            }
-        })).execute(url);
+        this.img = bitmap;
     }
 
     private static ArrayList<String> getUrls(long startId, int count) {
         //TODO
-        return null;
+        ArrayList<String> test = new ArrayList<>();
+        test.add("https://memepedia.ru/wp-content/uploads/2016/08/med_1426704578_00014.jpg");
+        test.add("http://vsekidki.ru/uploads/posts/2016-01/1453764864_l_c0788aa1.jpg");
+        test.add("http://8uh.ru/wp-content/uploads/2015/10/chto-takoe-kek-v-vkontakte.jpg");
+        return test;
     }
 
     public static ArrayList<Meme> getMemes(long startId, int count) {
         List<String> stringList = getUrls(startId, count);
         if (stringList == null)
             return null;
-        ArrayList<Meme> memeArrayList = new ArrayList<>();
-        for (String url : stringList) {
-            memeArrayList.add(new Meme(startId, url));
-            startId--;
+        memes = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            memes.add(new Meme(startId - i, stringList.get(i), null));
         }
-        return memeArrayList;
+        (new MemeDownloader()).execute(stringList.toArray(new String[count]));
+        return memes;
     }
 
-    Bitmap getImage() {
+    public Bitmap getImage() {
         return img;
     }
 
-    private static class MemeDownloader extends AsyncTask<String, Void, Bitmap> {
-
-        private final OnImageComplete onImageComplete;
-
-        private MemeDownloader(OnImageComplete onImageComplete) {
-            this.onImageComplete = onImageComplete;
-        }
+    private static class MemeDownloader extends AsyncTask<String, Void, Void> {
 
         @Override
         protected void onPreExecute() {
@@ -63,34 +57,25 @@ public final class Meme {
         }
 
         @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            if (bitmap != null) {
-                if (onImageComplete != null)
-                    onImageComplete.onComplete(bitmap);
-            }
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
         }
-
 
         @Override
-        protected Bitmap doInBackground(String... urls) {
-            if (urls[0] == null)
-                return null;
-            try {
-                URL url = new URL(urls[0]);
-                URLConnection connection = url.openConnection();
-                connection.connect();
-                return BitmapFactory.decodeStream(connection.getInputStream());
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+        protected Void doInBackground(String[] urls) {
+            for (int i = 0; i < urls.length; i++) {
+                try {
+                    URL url = new URL(urls[i]);
+                    URLConnection connection = url.openConnection();
+                    connection.connect();
+                    memes.get(i).img = BitmapFactory.decodeStream(connection.getInputStream());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             return null;
-        }
-
-        private interface OnImageComplete {
-            void onComplete(Bitmap image);
         }
     }
 }
